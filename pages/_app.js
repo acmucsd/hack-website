d97372e80ba9b8f4603254a9dd1422a923ab1272
Nishant Balaji <nishantb1130@gmail.com>
2023-07-31 23:04:05 -0700

    fix linting

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,12 @@
-import { DM_Sans } from 'next/font/google'
- 
+import { DM_Sans } from 'next/font/google';
+
 // If loading a variable font, you don't need to specify the font weight
-const inter = DM_Sans({ subsets: ['latin'],
-weight: '400'})
- 
+const font = DM_Sans({ subsets: ['latin'], weight: '400' });
+
 export default function MyApp({ Component, pageProps }) {
   return (
-    <main className={inter.className}>
+    <main className={font.className}>
       <Component {...pageProps} />
     </main>
-  )
+  );
 }
