9908b0469653be87fff8e36ba51f8a8e0e31d848
Angela Hu <51968178+angela139@users.noreply.github.com>
2023-09-28 22:54:31 -0700

    Events Section (#31)
    * Set up structure for homepage
    
    * Added landing card and restructured
    
    * Added starter code for components
    
    * Fixed the scroll for arrow icon + updated components template code
    
    * Fixing yarn stuff
    
    * Team cards added to homepage
    
    * Made page responsive and minor fixes
    
    * Fixed errors for Alexis changes
    
    * Dark mode isn't set up yet
    
    * Fixed index key error during vercel build (#25)
    
    Co-authored-by: avVergnet <avergnet@ucsd.edu>
    
    * Minor cleanup
    
    * Improved flexbox
    
    * Accidentally deleted the wrong code
    
    * Title doesn't move with typewriter
    
    * Created events card
    
    * Fixed bug with layout in safari
    
    * Smoother for mobile/tablet
    
    * Added photos and changed styling for event card
    
    * Last bit of styling
    
    * Remove react for now
    
    ---------
    
    Co-authored-by: avVergnet <avergnet@ucsd.edu>
    Co-authored-by: AVERGNET <47065067+AVERGNET@users.noreply.github.com>

--- src/homepage.tsx
+++ src/homepage.tsx
@@ -5,12 +5,17 @@ import About from './sections/About';
 import Events from './sections/Events';
 import Team from './sections/Team';
 
-const HomePage: NextPage = () => {
+import { EventsArray } from './api/events_api';
+
+const HomePage: NextPage<{ past_events: EventsArray; future_events: EventsArray }> = ({
+  past_events,
+  future_events,
+}) => {
   return (
     <main>
       <Hero />
       <About />
-      <Events />
+      <Events past_events={past_events} future_events={future_events} />
       <Team />
     </main>
   );
--- src/sections/Events/index.tsx
+++ src/sections/Events/index.tsx
@@ -1,7 +1,48 @@
+import { useState } from 'react';
 import styles from './style.module.css';
+import EventsCard from '../../components/event-card';
+import { EventsArray } from '../../api/events_api';
 
-const Events: React.FC = () => {
-  return <h3 className={styles.events}>Event Section</h3>;
+const Events: React.FC<{ past_events: EventsArray; future_events: EventsArray }> = ({
+  past_events,
+  future_events,
+}) => {
+  const [selectedOption, setSelectedOption] = useState('Upcoming'); // State to store the selected option
+  const [filteredEvents, setFilteredEvents] = useState(future_events);
+
+  // Function to handle option change
+  const handleOptionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selected = event.target.value;
+    setSelectedOption(selected);
+
+    // Set filtered events based on the selected option
+    if (selected === 'Upcoming') {
+      setFilteredEvents(future_events);
+    } else if (selected === 'Past') {
+      setFilteredEvents(past_events);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <h1 className={styles.title}>Events</h1>
+        <div className={styles.event_type}>
+          <select onChange={handleOptionChange} value={selectedOption}>
+            <option value="Upcoming">Upcoming</option>
+            <option value="Past">Past</option>
+          </select>
+        </div>
+      </div>
+      <div className={styles.events}>
+        {!filteredEvents ? (
+          <h3>No upcoming events. Check again later!</h3>
+        ) : (
+          filteredEvents.map(event => <EventsCard key={event.uuid} event={event} />)
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Events;
