c586dc82c7ae32514fe7d262b4e635cd4c4b70a4
Uliyaah Dionisio <uliyaah@gmail.com>
2024-01-22 16:40:10 -0800

    Code formatting

--- src/sections/EventsHub/index.tsx
+++ src/sections/EventsHub/index.tsx
@@ -3,14 +3,13 @@ import styles from './style.module.css';
 import WorkshopCards from '../../components/workshop-card/';
 import { EventObject, getAllHackEvents } from '../../api/events_api';
 
-const EventsHub: React.FC<{latest_events: EventObject[]}> = ({}) => {
+const EventsHub: React.FC<{ latest_events: EventObject[] }> = () => {
   const [selectedYear, setSelectedYear] = useState<string | null>('2023');
   const [hackEvents, setHackEvents] = useState<EventObject[] | undefined>(undefined);
 
   const handleYearChange = async (event: React.ChangeEvent<HTMLSelectElement>) => {
-    
     setSelectedYear(event.target.value);
-    
+
     try {
       const events = await getAllHackEvents('');
       setHackEvents(events);
@@ -19,32 +18,28 @@ const EventsHub: React.FC<{latest_events: EventObject[]}> = ({}) => {
     }
   };
 
-
-
-    return (
-      <div className={styles.container}>
-        <div className={styles.header}>
-            <h1 className={styles.title}>Events Hub</h1>
-            <div className={styles.event_type}>
-              <select
-                className={styles.event_select}
-                id="year" 
-                onFocus={handleYearChange}
-                onChange={handleYearChange} 
-                value={selectedYear || '2023'}
-              >
-                <option value='2023'>Hack School 2023</option>
-                <option value='2022'>Hack School 2022</option>
-                <option value='2021'>Hack School 2021</option>
-                <option value='2020'>Hack School 2020</option>
-              </select>
-            </div>
-        </div>
-        <div>
-        <WorkshopCards year={selectedYear} workshops={hackEvents || []} />
-        </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+          <h1 className={styles.title}>Events Hub</h1>
+          <div className={styles.event_type}>
+            <select className={styles.event_select}
+            id="year" 
+            onChange={handleYearChange} 
+            value={selectedYear || '2023'}
+            >
+              <option value="2023">Hack School 2023</option>
+              <option value="2022">Hack School 2022</option>
+              <option value="2021">Hack School 2021</option>
+              <option value="2020">Hack School 2020</option>
+            </select>
+          </div>
       </div>
-    );
-  };
-  
-  export default EventsHub;
+      <div>
+        <WorkshopCards year={selectedYear} workshops={hackEvents || []} />
+      </div>
+    </div>
+  );
+};
+
+export default EventsHub;
