437c53de346d58f5afffd3f4dec219abcc05f279
Tyler <tylerhoang456123@gmail.com>
2025-01-08 14:51:50 -0800

    navbar progress: navigation sections, start work replicating

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,7 +1,49 @@
+
+// based on navbar from acmucsd.com
+// https://github.com/acmucsd/main-website/blob/main/src/components/NavigationBar/index.tsx
+
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
 import styles from './style.module.css';
+import Logo from '../../../public/Logo';
+
+const navLinks = [
+  { to: "", text: "About" },
+  { to: "", text: "Events" },
+  { to: "", text: "Meet the Team"},
+  { to: "", text: "Hack School" }
+]
 
 const Navbar: React.FC = () => {
-  return <h3>Navbar</h3>;
+  const size: Size = useWindowSize();
+  
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (<div></div>);
 };
+
+interface Size {
+  width: number | undefined;
+  height: number | undefined;
+}
+
+function useWindowSize(): Size {
+  const [windowSize, setWindowSize] = useState<Size>({
+    width: undefined,
+    height: undefined,
+  });
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  return windowSize; 
+}
 
 export default Navbar;
