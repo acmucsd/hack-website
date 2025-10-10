98c36d4f8eb45b395db636e9100bb6be494e36df
Ayush Shah <97317706+Ayush1124@users.noreply.github.com>
2025-10-09 18:00:25 -0700

    Ayush/hack changes (#90)
    * Renamed weeks
    
    * navbar changes
    
    * formatting changes
    
    * Swapped facebook for youtube

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -47,13 +47,17 @@ const Navbar: React.FC = () => {
         // Not on home, navigate to home with hash
         router.push(href);
       }
+    } else {
+      setIsMenuOpen(false);
     }
   };
 
   return (
     <nav className={styles.navbar}>
       <div className={styles.navbarContent}>
-        <Logo />
+        <Link href="/" onClick={() => setIsMenuOpen(false)} aria-label="Home">
+          <Logo />
+        </Link>
         <button
           type="button"
           className={`${styles.menuToggle} ${isMenuOpen ? styles.menuToggleOpen : ''}`}
@@ -82,7 +86,9 @@ const Navbar: React.FC = () => {
                   {link.name}
                 </button>
               ) : (
-                <Link href={link.href}>{link.name}</Link>
+                <Link href={link.href} onClick={() => setIsMenuOpen(false)}>
+                  {link.name}
+                </Link>
               )}
             </li>
           ))}
